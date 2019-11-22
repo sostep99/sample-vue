0be53f377f1b6260bd88b08bb813dfffa8760a97
sam <sostep99@harrisonburg.k12.va.us>
2019-11-22 09:49:27 -0500

    fdsfd

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Stats from "../views/Stats.vue";
+import LeaderBoard from "../views/LeaderBoard.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -18,6 +19,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/",
+    name: "Stats",
+    component: Stats
+  },
+  {
+    path: "/",
+    name: "LeaderBoard",
+    component: LeaderBoard
   }
 ];
 
